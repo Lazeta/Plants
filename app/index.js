ba208f11e20863ea7b6b1327334d7b46e0c0966f
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-05-09 20:02:48 +0300

    feat: realize smooth scroll to anchor into mobile screen less 769px, thansition from burger meni with closure burger menu and hiding the the cross menu icon

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
-// функция смены цвета заднего фона на размерах от 1440 пикселей более.
-// function changed color theme on size from 1440px and more.
+// Функция смены цвета заднего фона на размерах от 1440 пикселей более.
+// Function changed color background color on size from 1440px and more.
 const btnColor = document.querySelector('.__changer');
 function random(number){
     return Math.floor(Math.random() * (number+1))
@@ -10,25 +10,37 @@ btnColor.onclick = function(){
 };
 
 
-// для бургер меню из 4 полос где по нажатия всплывает окно навигации по сайту.
+
+// Для бургер меню из 4 полос где по нажатия всплывает окно навигации по сайту.
+// For a burger menu of 4 lanes where, when clicked, a site navigation window pops up.
+// Фиксируем константы нашего меню навигации и иконки меню.
+// We fixed the constants of our navigation menu and menu icon.
 const burgerIcon = document.querySelector('.burger__menu');
 const burgerMenu = document.querySelector('.navigation')
 
-burgerIcon.addEventListener("click", () => {
+
+// Получаем кнопку меню бургер и добавляем обработчик события click,
+// Get the burger menu button and add a click event handler,
+// где добавляем или удаляем класс в зависимости от того есть он уже или нет.
+// where we add or remove a class, depending on whether it already exist or not.
+burgerIcon.addEventListener("click", function () {
     burgerIcon.classList.toggle('__active');
-    document.querySelector('.navigation').classList.toggle('__burger__menu');
+    burgerMenu.classList.toggle('__burger__menu');
 });
 
-// при нажатии на Escape можно скрыть меню
+
+// Добавляем обработчик нажатия на клавишу Escape для закрытия меню.
+// Add a handler for pressing the Escape key to close the menu.
 document.addEventListener('keydown', function(e) {
     if(e.key === 'Escape' || e.key === 'Esc') { 
         burgerMenu.classList.remove('__burger__menu');
-         burgerIcon.classList.remove('__active');
+        burgerIcon.classList.remove('__active');
     };
 });
 
 
-// будем скрывать меню если кликать вне списка за его пределами.
+// Будем скрывать меню если кликать вне списка за его пределами.
+// We will hide the menu if you click outside the list outside of it.
 document.addEventListener('click', function(e) {
     if (!burgerIcon.contains(e.target) && !burgerMenu.contains(e.target)){
         burgerMenu.classList.remove('__burger__menu');
@@ -36,5 +48,47 @@ document.addEventListener('click', function(e) {
     }
 })
 
-// написать скрипт кнопки что будет возвращать пользователей к шапке по 
-// id="Header" и исчезать когда пользователь находится в верху страницы.
+
+// Функция плавного скроллинга к якорю
+// Function of smooth scrolling to the anchor
+function smoothScroll(target) {
+    if (target) {
+      let element = document.querySelector(target);
+      if (element) {
+        element.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+
+
+// Данный обработчик будет действовать на разрешении экрана менее 
+// This handler will operate at screen resolutions less than
+// или равный 768 пикселей.
+// or equal to 768 pixels.
+if (window.innerWidth <= 768) {
+    // Получаем ссылки меню и добавляем обработчик события click.
+    // Get the menu links and add a click event handler.
+    const menuLinks = document.querySelectorAll('.menu-link');
+    for (let i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].addEventListener('click', function(e) {
+        // Закрываем меню.
+        // Close the menu.
+        if (burgerMenu.classList.contains('__burger__menu')) {
+        burgerMenu.classList.remove('__burger__menu') }
+        if (burgerIcon.classList.contains('__active')) {
+            burgerIcon.classList.remove('__active') }
+        // Получаем якорь и осуществляем переход.
+        // We get the anchor and make the thansition.
+        const target = this.getAttribute('href');
+        const targetElement = document.querySelector(target);
+        if (targetElement) {
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+            });
+        }
+        e.preventDefault();
+        });
+    }
+}
