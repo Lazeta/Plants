bdedf90c709b089e68560fc7c10d3c80ffac0ac7
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-05-18 14:47:48 +0300

    feat: create architecture by pattern 7-1 parth-2

--- app/index.js
+++ app/index.js
@@ -1,16 +1,3 @@
-// *RU* Функция смены цвета заднего фона на размерах от 1440 пикселей более.
-// Function changed color background color on size from 1440px and more.
-const btnColor = document.querySelector('.__changer');
-function random(number){
-    return Math.floor(Math.random() * (number+1))
-};
-btnColor.onclick = function(){
-    const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-    document.body.style.backgroundColor = rndCol;
-};
-
-
-
 /* *RU* Данный обработчик будет действовать на разрешении экрана менее 
 или равный 768 пикселей.
 This handler will operate at screen resolutions less than
