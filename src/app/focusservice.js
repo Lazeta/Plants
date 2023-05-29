ff17ef9b2e01e2c1dc9f7940aae5fe059db46ce2
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-05-29 19:51:56 +0300

    feat: implement blur and focus for service section, but we have bag bluring if click other button

--- src/app/focusservice.js
+++ src/app/focusservice.js
@@ -1,1 +1,28 @@
-const n = '';
+// берём кнопки и предметы обслуживания
+// get the btn and service items
+const btn = document.querySelectorAll("button");
+const serviceItems = document.querySelectorAll(".service__item");
+
+// добавляем события прослушивания кнопок
+// add event listeners to the btn
+btn.forEach(button => {
+    button.addEventListener('click', () => {
+        // get the name of the button
+        const name = button.innerText;
+
+        // Loop through the service items
+        serviceItems.forEach(item => {
+            // check if the service item has the same id as the button name
+            if (item.id === name) {
+                // add the 'focused' class to the service item
+                item.classList.add('focused');
+                item.classList.remove('active');
+            }
+            else {
+                // remove the 'focused' class from other service items
+                item.classList.remove('focused');
+                item.classList.add('active');
+            }
+        });
+    });
+});
