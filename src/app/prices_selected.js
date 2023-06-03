460ee25b992c8ee4cc88abd9ab1493262c3bc547
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-06-03 19:20:36 +0300

    feat: refactoring architecture price__bar__content, updated styles for normal state and selected state; start add script for accordions

--- src/app/prices_selected.js
+++ src/app/prices_selected.js
@@ -1,1 +1,14 @@
-const accordion = document.querySelectorAll('.accordion__item')
+const pricesItems = document.querySelectorAll('.prices__item');
+
+pricesItems.forEach(item => {
+
+    const accordion = item.querySelector('.accordion');
+    const accordionHeader = document.querySelector('.accordion__header');
+    const accordionContent = document.querySelector('.accordion__content');
+    const arrow = document.querySelector('.arrow');
+    const arrowButton = document.querySelector('.arrow-bottom');
+
+    item.addEventListener('click', () => {
+        
+    })
+});
