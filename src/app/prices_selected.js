6b9bbf0308b584fbf3635b2e51306455701de0f9
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-06-04 00:01:25 +0300

    feat: implement opened accordion menu

--- src/app/prices_selected.js
+++ src/app/prices_selected.js
@@ -1,14 +1,16 @@
 const pricesItems = document.querySelectorAll('.prices__item');
 
-pricesItems.forEach(item => {
-
+pricesItems.forEach((item) => {
     const accordion = item.querySelector('.accordion');
-    const accordionHeader = document.querySelector('.accordion__header');
-    const accordionContent = document.querySelector('.accordion__content');
-    const arrow = document.querySelector('.arrow');
-    const arrowButton = document.querySelector('.arrow-bottom');
+    const accordionHeader = accordion.querySelector('.accordion__header');
+    const accordionContent = accordion.querySelector('.accordion__content');
+    const arrow = accordion.querySelector('.arrow');
+    const arrowButton = accordion.querySelector('.arrow-bottom');
 
     item.addEventListener('click', () => {
-        
-    })
+        accordionHeader.classList.toggle('open');
+        arrow.classList.toggle('open');
+        arrowButton.classList.toggle('closes');
+        accordionContent.classList.toggle('selected');
+    });
 });
