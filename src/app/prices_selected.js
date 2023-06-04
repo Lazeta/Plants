cadfc4dddd28106e03968c7cd730aeb36ab086ae
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-06-04 18:44:25 +0300

    feat: prices_selected.js add close previous menu prise__item

--- src/app/prices_selected.js
+++ src/app/prices_selected.js
@@ -1,34 +1,89 @@
-const pricesItems = document.querySelectorAll('.prices__item');
+// // получаем все элементы с классом "prices__item" и добавляем обработчик события клика
+// document.querySelectorAll(".prices__item").forEach((item) => {
+//     item.addEventListener('click', () => {
+//         // Получаем элементы меню: заголовок, содержимое и стрелки
+//         const accordionHeader = item.querySelector('.accordion__header');
+//         const accordionContent = item.querySelector('.accordion__content');
+//         const accordionArrowTop = item.querySelector('.accordion__header__selected');
 
-pricesItems.forEach((item) => {
-    const accordion = item.querySelector('.accordion');
-    const accordionHeader = item.querySelector('.accordion__header');
-    const accordionContent = item.querySelector('.accordion__content');
-    const arrow = item.querySelector('.arrow');
+//         // Проверяем, открыто ли меню
+//         const isAccordionActive = accordionHeader.classList.contains('accordion__header__selected');
 
-    item.addEventListener('click', () => {
-        if (accordion.classList.contains('active')) {
-            accordionContent.classList.remove('selected');
-            accordionHeader.classList.remove('accordion__header__selector');
-            accordionHeader.classList.add('accordion__header');
-            arrow.classList.remove('open');
-            accordion.classList.remove('active');
+//         if (isAccordionActive) {
+//             // Если меню уде открыто, то закрываем его и удаляем соответствующие классы
+//             accordionHeader.classList.remove('accordion__header__selected');
+//             accordionContent.classList.remove('selected');
+//         } 
+
+//         else {
+//             // Если меню закрыто, то закрываем предыдущее открытое меню, если имеется в наличии
+//             const activeAccordion = document.querySelector('.prices__item .accordion__header__selected');
+
+//             const activeAccordionHeader = activeAccordion.parentNode.querySelector('.accordion__header');
+//             const activeAccordionContent = activeAccordion.parentNode.querySelector('.accordion__content');
+//             const activeAccordionArrowBottom = activeAccordion.parentNode.querySelector('.accordion__header');
+
+//             // Открываем текущее меню и добавляем соответствующие классы
+//             accordionHeader.classList.add('accordion__header__selected');
+//             accordionContent.classList.add('selected');    
+//         }
+
+//         window.addEventListener('keydown', function (event) {
+//             if (event.key === 'Escape' || event.key === 'Esc') {
+//                 accordionContent.classList.remove('selected');
+//                 accordionHeader.classList.remove('accordion__header__selected');
+//             }
+//         });
+//     });
+// })
+
+// получаем все элементы с классом "prices__item" и добавляем обработчик события клика
+document.querySelectorAll(".prices__item").forEach((item) => {
+    item.addEventListener("click", () => {
+        // Получаем элементы меню: заголовок, содержимое и стрелки
+        const accordionHeader = item.querySelector(".accordion__header");
+        const accordionContent = item.querySelector(".accordion__content");
+        const accordionArrowTop = item.querySelector(".arrow-top");
+
+        // Проверяем, открыто ли меню
+        const isAccordionActive = accordionHeader.classList.contains("accordion__header__selected");
+
+        if (isAccordionActive) {
+            // Если меню уже открыто, то закрываем его и удаляем соответствующие классы
+            accordionHeader.classList.remove("accordion__header__selected");
+            accordionContent.classList.remove("selected");
         } else {
-            accordionContent.classList.add('selected');
-            accordionHeader.classList.remove('accordion__header__selector');
-            accordionHeader.classList.add('accordion__header');
-            arrow.classList.add('open');
-            accordion.classList.add('active');
-        }
-    });
+            // Если меню закрыто, то закрываем предыдущее открытое меню, если имеется в наличии
+            const activeAccordion = document.querySelector(".prices__item .accordion__header__selected");
 
-    window.addEventListener('keydown', function (event) {
-        if (event.key === 'Escape' || event.key === 'Esc') {
-            accordion.classList.remove('active');
-            accordionContent.classList.remove('selected');
-            accordionHeader.classList.remove('accordion__header__selector');
-            accordionHeader.classList.add('accordion__header');
-            arrow.classList.remove('open');
+            if (activeAccordion) {
+                const activeAccordionItem = activeAccordion.closest(".prices__item");
+                const activeAccordionHeader = activeAccordionItem.querySelector(".accordion__header");
+                const activeAccordionContent = activeAccordionItem.querySelector(".accordion__content");
+                const activeAccordionArrowTop = activeAccordionItem.querySelector(".arrow-top");
+
+                // Проверяем наличие активного меню перед удалением класса
+                if (activeAccordionHeader) {
+                    activeAccordionHeader.classList.remove("accordion__header__selected");
+                }
+                if (activeAccordionContent) {
+                    activeAccordionContent.classList.remove("selected");
+                }
+                if (activeAccordionArrowTop) {
+                    activeAccordionArrowTop.style.display = "block";
+                }
+            }
+
+            // Открываем текущее меню и добавляем соответствующие классы
+            accordionHeader.classList.add("accordion__header__selected");
+            accordionContent.classList.add("selected");
         }
+
+        window.addEventListener("keydown", function (event) {
+            if (event.key === "Escape" || event.key === "Esc") {
+                accordionContent.classList.remove("selected");
+                accordionHeader.classList.remove("accordion__header__selected");
+            }
+        });
     });
 });
