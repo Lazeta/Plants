306adc536ae1eb53fc82fe2e4b868d30bb94e2a8
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-06-04 05:31:44 +0300

    fix: fixed number click for event

--- src/app/prices_selected.js
+++ src/app/prices_selected.js
@@ -1,41 +1,34 @@
 const pricesItems = document.querySelectorAll('.prices__item');
 
-
 pricesItems.forEach((item) => {
+    const accordion = item.querySelector('.accordion');
+    const accordionHeader = item.querySelector('.accordion__header');
+    const accordionContent = item.querySelector('.accordion__content');
+    const arrow = item.querySelector('.arrow');
+
     item.addEventListener('click', () => {
-        const accordion = item.querySelector('.accordion');
-        const accordionHeader = item.querySelector('.accordion__header');
-        const accordionContent = item.querySelector('.accordion__content');
-        const arrow = item.querySelector('.arrow');
-        const arrowButton = item.querySelector('.arrow-bottom');
-        
-        window.addEventListener('keydown', function(event) {
-            if (event.key === 'Escape' || event.key === 'Esc') {
-                accordion.classList.remove('active');
-                accordionContent.classList.remove('selected');
-                accordionHeader.classList.remove('close');
-                arrow.classList.remove('open');
-                arrowButton.classList.remove('close');
-    
-            }
-        });
-
-
-
         if (accordion.classList.contains('active')) {
             accordionContent.classList.remove('selected');
-            accordionHeader.classList.remove('close');
+            accordionHeader.classList.remove('accordion__header__selector');
+            accordionHeader.classList.add('accordion__header');
             arrow.classList.remove('open');
-            arrowButton.classList.remove('close');
-            accordion.classList.toggle('active');
-        }
-        else {
+            accordion.classList.remove('active');
+        } else {
             accordionContent.classList.add('selected');
-            accordionHeader.classList.add('close');
+            accordionHeader.classList.remove('accordion__header__selector');
+            accordionHeader.classList.add('accordion__header');
             arrow.classList.add('open');
-            arrowButton.classList.add('close');
-            accordion.classList.toggle('active');
+            accordion.classList.add('active');
         }
     });
 
+    window.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape' || event.key === 'Esc') {
+            accordion.classList.remove('active');
+            accordionContent.classList.remove('selected');
+            accordionHeader.classList.remove('accordion__header__selector');
+            accordionHeader.classList.add('accordion__header');
+            arrow.classList.remove('open');
+        }
+    });
 });
