b8298e7b3951ef10d5899e7f00df8cc1df161b9f
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-06-10 22:45:09 +0300

    feat: city_selector, _contact-us__selected, index.html the new structure drop-menu city (Contact us selction)

--- src/app/city_selected.js
+++ src/app/city_selected.js
@@ -1,7 +1,11 @@
 const cityBtn = document.querySelector(".contact-us__selected")
 const cityBtnOld = document.querySelector(".btn__city__bar");
 
-cityBtn.addEventListener("click", function () {
-    cityBtnOld.classList.toggle("_active");
-    // cityBtn.classList.toggle("_active");
-})
+if (cityBtn.classList.contains("contact-us__selected._active")) {
+
+} else {
+    cityBtn.addEventListener("click", function () {
+        cityBtnOld.classList.toggle("_active");
+        // cityBtn.classList.toggle("_active");
+    })
+}
