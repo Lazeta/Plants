380d1c2181c1b8959dac061b2feccbb76a20f057
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-06-14 01:01:54 +0300

    feat: implement function mouse over for button city hover that open city list

--- src/app/city_selected.js
+++ src/app/city_selected.js
@@ -8,7 +8,7 @@ cityBtn.addEventListener("click", function () {
     cityList.classList.toggle("_active");
 });
 
- 
+
 window.addEventListener("keydown", function (event) {
     if (event.key === "Escape" || event.key === "Esc") {
         cityBtn.classList.remove("_active");
@@ -18,17 +18,31 @@ window.addEventListener("keydown", function (event) {
     }
 });
 
+function mouseOverButton() {
+    let cityList = document.querySelector(".city-selection__bar");
+    let cityBtn = document.querySelector(".btn__city__bar");
 
+    cityBtn.addEventListener("mouseout", function (event) {
+        cityList.classList.remove("_active");
+    }); 
+    cityBtn.addEventListener("mouseover", function(event) {
+        cityList.classList.add("_active"); 
+    });
+}
+
+function mouseOverList() {
+
+}
 
 itemCity.forEach(function (item) {
-    
-    item.addEventListener("click", function() {
+
+    item.addEventListener("click", function () {
         const elem1 = document.querySelector(".Canandaigua");
         const elem2 = document.querySelector(".New-York-City");
         const elem3 = document.querySelector(".Yonkers");
         const elem4 = document.querySelector(".Sherrill");
 
-        window.addEventListener("keydown", function(e) {
+        window.addEventListener("keydown", function (e) {
             if (e.key === "Escape" || e.key === "Esc") {
                 elem1.classList.remove("_init");
                 elem2.classList.remove("_init");
@@ -37,21 +51,24 @@ itemCity.forEach(function (item) {
             }
         });
 
-        if (elem1.classList.contains('_init') || elem2.classList.contains('_init') || elem3.classList.contains('_init') || elem4.classList.contains('_init')){
+        if (elem1.classList.contains('_init') || elem2.classList.contains('_init') || elem3.classList.contains('_init') || elem4.classList.contains('_init')) {
             elem1.classList.remove("_init");
             elem2.classList.remove("_init");
             elem3.classList.remove("_init");
             elem4.classList.remove("_init");
         }
-        if(item.classList.contains("city-name__Canandaigua")) {
+        if (item.classList.contains("city-name__Canandaigua")) {
+            cityList.classList.add("_hidden");
             cityBtn.classList.add("_hidden");
             elem1.closest(".city__card").classList.add("_init");
-            return item 
+            return item
         } else if (item.classList.contains('city-name__New-York-City')) {
+            cityList.classList.add("_hidden");
             cityBtn.classList.add("_hidden");
             elem2.closest(".city__card").classList.add("_init");
             return item
         } else if (item.classList.contains('city-name__Yonkers')) {
+            cityList.classList.add("_hidden");
             cityBtn.classList.add("_hidden");
             elem3.closest(".city__card").classList.add("_init");
             return item
@@ -60,8 +77,6 @@ itemCity.forEach(function (item) {
             cityBtn.classList.toggle("_hidden");
             elem4.closest(".city__card").classList.add("_init");
             return item
-        } else {
-            console.log("this element does not exist!");
-        }
+        } else console.log("this element does not exist!");
     });
 });
